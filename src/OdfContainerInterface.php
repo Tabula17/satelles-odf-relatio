eb8abac53c5760ca501991b0a270eca1393d6692
Martín <820694+Martin17@users.noreply.github.com>
2025-05-21 09:02:18 -0300

    Remove return type declarations from OdfContainerInterface methods for improved flexibility

--- src/OdfContainerInterface.php
+++ src/OdfContainerInterface.php
@@ -41,9 +41,8 @@ interface  OdfContainerInterface
     /**
      * @param string $fileName
      * @param $mime
-     * @return void
      */
-    public function registerFileInManifest(string $fileName, $mime): void;
+    public function registerFileInManifest(string $fileName, $mime);
 
     /**
      * Adds an image file to the ODT file by including it in the 'Pictures' directory inside the archive.
@@ -51,10 +50,9 @@ interface  OdfContainerInterface
      * @param string $imgPath The path to the image file that should be added.
      * @param string|null $name An optional name for the image file within the archive.
      *                          If not provided, the basename of the $imgPath will be used.
-     * @return void Returns the current instance for method chaining.
      * @throws RuntimeException If the ODT file cannot be opened.
      */
-    public function addImage(string $imgPath, ?string $name = null): void;
+    public function addImage(string $imgPath, ?string $name = null);
 
     /**
      * Adds multiple image files to the ODT file by including them in the 'Pictures' directory inside the archive.
@@ -62,10 +60,9 @@ interface  OdfContainerInterface
      * @param array $imgPaths An array of paths to the image files that should be added.
      * @param array|null $name An optional array of names for the image files within the archive.
      *                          If not provided, the basename of each $imgPath will be used.
-     * @return void Returns the current instance for method chaining.
      * @throws RuntimeException If the ODT file cannot be opened.
      */
-    public function addImages(array $imgPaths, ?array $name = null): void;
+    public function addImages(array $imgPaths, ?array $name = null);
 
-    public function saveFile(): void;
+    public function saveFile();
 }
